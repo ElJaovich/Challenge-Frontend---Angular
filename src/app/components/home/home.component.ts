5d5497d189d5a69e5d35a2567ea449933d52ce99
master360 <18-10641@usb.ve>
2022-07-19 13:34:10 -0400

    -

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,6 +16,7 @@ export class HomeComponent implements OnInit {
   public veganHealthScore: number = 0
   public normalDish: any
   public veganDish: any
+  public deleted: boolean = true 
 
   constructor(
     private dish:DishService
@@ -29,7 +30,6 @@ export class HomeComponent implements OnInit {
 
       this.normalDish = await this.dish.getNormalDish()
       this.veganDish = await this.dish.getVeganDish()
-
       for (let i = 0; i < this.normalDish.length; i++) {
         //console.log(normalDish[i].price)
         //console.log(normalDish[i].normalHealthScore)
@@ -37,9 +37,9 @@ export class HomeComponent implements OnInit {
         this.normalPriceDish = this.normalPriceDish + this.normalDish[i].price
         this.normalHealthScore = this.normalHealthScore + this.normalDish[i].healthScore
       }
-      console.log('normalTimeDish:',this.normalTimeDish/this.normalDish.length, 'minutes')
-      console.log('normalHealthScore media:', this.normalHealthScore/this.normalDish.length)
-      console.log('total price',this.normalPriceDish)
+      //console.log('normalTimeDish:',this.normalTimeDish/this.normalDish.length, 'minutes')
+      //console.log('normalHealthScore media:', this.normalHealthScore/this.normalDish.length)
+      //console.log('total price',this.normalPriceDish)
 
       for (let i = 0; i < this.veganDish.length; i++) {
         this.veganTimeDish = this.veganTimeDish + this.veganDish[i].timeReady
@@ -47,9 +47,18 @@ export class HomeComponent implements OnInit {
         this.veganHealthScore = this.veganHealthScore + this.veganDish[i].healthScore
         //console.log(veganDish[i].price)
       }
-      console.log('veganTimeDish:',this.veganTimeDish/this.veganDish.length, 'minutes')
-      console.log('veganHealthScore media:', this.veganHealthScore/this.veganDish.length)
-      console.log('total price',this.veganPriceDish)
+      //console.log('veganTimeDish:',this.veganTimeDish/this.veganDish.length, 'minutes')
+      //console.log('veganHealthScore media:', this.veganHealthScore/this.veganDish.length)
+     //console.log('total price',this.veganPriceDish)
+    }
+
+    deleteDish(info:any){
+      console.log(info.id)
+      const result = this.normalDish.splice(0,info.id )
+      console.log(result)
+      
+      //this.deleted = !this.deleted
+
     }
 
 }
